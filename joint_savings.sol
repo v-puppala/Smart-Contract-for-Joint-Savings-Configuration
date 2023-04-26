33c82cb59bc913e302e3d6fa21eba3a60364b6e2
v-puppala <puppalavishal@wustl.edu>
2023-04-26 13:15:43 -0500

    withdraw function

--- joint_savings.sol
+++ joint_savings.sol
@@ -16,53 +16,55 @@ You will do the following:
 */
 
 pragma solidity ^0.5.0;
+//hi
 
 // Define a new contract named `JointSavings`
 contract JointSavings {
+    address payable accountOne;
+    address payable accountTwo;
+    address public lastToWithdraw;
+    uint public lastWithdrawAmount;
+    uint public contractBalance;
 
-    /*
-    Inside the new contract define the following variables:
-    - Two variables of type `address payable` named `accountOne` and `accountTwo`
-    - A variable of type `address public` named `lastToWithdraw`
-    - Two variables of type `uint public` named `lastWithdrawAmount` and `contractBalance`.
-    */
-    // YOUR CODE HERE!
+ 
 
-    /*
-    Define a function named **withdraw** that will accept two arguments.
-    - A `uint` variable named `amount`
-    - A `payable address` named `recipient`
-    */
     function withdraw(uint amount, address payable recipient) public {
 
         /*
         Define a `require` statement that checks if the `recipient` is equal to either `accountOne` or `accountTwo`. The `requiere` statement returns the text `"You don't own this account!"` if it does not.
         */
         // YOUR CODE HERE!
+        require(recipient==accountOne || recipient==accountTwo,"You don't own this account!");
 
         /*
         Define a `require` statement that checks if the `balance` is sufficient to accomplish the withdraw operation. If there are insufficient funds, the text `Insufficient funds!` is returned.
         */
         // YOUR CODE HERE!
-
+        require(amount>contractBalance,"Insufficient funds!");
         /*
         Add and `if` statement to check if the `lastToWithdraw` is not equal to (`!=`) to `recipient` If `lastToWithdraw` is not equal, then set it to the current value of `recipient`.
         */
         // YOUR CODE HERE!
+        if (lastToWithdraw!=recipient) {
+            lastToWithdraw=recipient;
+        }
 
         // Call the `transfer` function of the `recipient` and pass it the `amount` to transfer as an argument.
         // YOUR CODE HERE!
-
+        recipient.transfer(amount);
         // Set  `lastWithdrawAmount` equal to `amount`
         // YOUR CODE HERE!
-
+        lastWithdrawAmount=amount;
         // Call the `contractBalance` variable and set it equal to the balance of the contract by using `address(this).balance` to reflect the new balance of the contract.
         // YOUR CODE HERE!
+        contractBalance=address(this).balance;
     }
 
     // Define a `public payable` function named `deposit`.
     function deposit() public payable {
 
+        contractBalance=address(this).balance
+        
         /*
         Call the `contractBalance` variable and set it equal to the balance of the contract by using `address(this).balance`.
         */
