96094ea9bd86292a411210b913ca40de2620d51c
v-puppala <puppalavishal@wustl.edu>
2023-04-26 22:16:40 -0500

    tidied things up

--- joint_savings.sol
+++ joint_savings.sol
@@ -1,19 +1,4 @@
-/*
-Joint Savings Account
----------------------
 
-To automate the creation of joint savings accounts, you will create a solidity smart contract that accepts two user addresses that are then able to control a joint savings account. Your smart contract will use ether management functions to implement various requirements from the financial institution to provide the features of the joint savings account.
-
-The Starting file provided for this challenge contains a `pragma` for solidity version `5.0.0`.
-You will do the following:
-
-1. Create and work within a local blockchain development environment using the JavaScript VM provided by the Remix IDE.
-
-2. Script and deploy a **JointSavings** smart contract.
-
-3. Interact with your deployed smart contract to transfer and withdraw funds.
-
-*/
 
 pragma solidity ^0.5.0;
 //hi
@@ -30,64 +15,42 @@ contract JointSavings {
 
     function withdraw(uint amount, address payable recipient) public {
 
-        /*
-        Define a `require` statement that checks if the `recipient` is equal to either `accountOne` or `accountTwo`. The `requiere` statement returns the text `"You don't own this account!"` if it does not.
-        */
-        // YOUR CODE HERE!
         require(recipient==accountOne || recipient==accountTwo,"You don't own this account!");
 
-        /*
-        Define a `require` statement that checks if the `balance` is sufficient to accomplish the withdraw operation. If there are insufficient funds, the text `Insufficient funds!` is returned.
-        */
-        // YOUR CODE HERE!
+
         require(amount>contractBalance,"Insufficient funds!");
-        /*
-        Add and `if` statement to check if the `lastToWithdraw` is not equal to (`!=`) to `recipient` If `lastToWithdraw` is not equal, then set it to the current value of `recipient`.
-        */
-        // YOUR CODE HERE!
+
         if (lastToWithdraw!=recipient) {
             lastToWithdraw=recipient;
         }
 
-        // Call the `transfer` function of the `recipient` and pass it the `amount` to transfer as an argument.
-        // YOUR CODE HERE!
+
         recipient.transfer(amount);
-        // Set  `lastWithdrawAmount` equal to `amount`
-        // YOUR CODE HERE!
+
         lastWithdrawAmount=amount;
-        // Call the `contractBalance` variable and set it equal to the balance of the contract by using `address(this).balance` to reflect the new balance of the contract.
-        // YOUR CODE HERE!
+
         contractBalance=address(this).balance;
     }
 
-    // Define a `public payable` function named `deposit`.
     function deposit() public payable {
 
         contractBalance=address(this).balance;
         
-        /*
-        Call the `contractBalance` variable and set it equal to the balance of the contract by using `address(this).balance`.
-        */
-        // YOUR CODE HERE!
+
     }
 
-    /*
-    Define a `public` function named `setAccounts` that receive two `address payable` arguments named `account1` and `account2`.
-    */
+
     function setAccounts(address payable account1, address payable account2) public{
 
-        // Set the values of `accountOne` and `accountTwo` to `account1` and `account2` respectively.
-        // YOUR CODE HERE!
         accountOne=account1;
         accountTwo=account2;
     }
 
     /*
-    Finally, add the **default fallback function** so that your contract can store Ether sent from outside the deposit function.
+    Default fallback function to collect remitances outside smart contract
     */
       function() external payable {
   }
 
 
-    // YOUR CODE HERE!
 }
